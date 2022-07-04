c89d8ba9944a6b5629369bd7f8a919db1b4e4880
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2022-07-03 21:17:40 -0400

    Transform in Class

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import pygame
 from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((1024, 720))
-pygame.display.set_caption('Pathfinder')
-clock = pygame.time.Clock()
+class Main:
+	def __init__(self, screenWidth, screenHeight, blockSize):
+		self.screenWidth = screenWidth
+		self.screenHeight = screenHeight
+		self.blockSize = blockSize
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
+	def initPygame(self, windowTitle):
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
+		pygame.display.set_caption(windowTitle)
+		self.clock = pygame.time.Clock()
 
-	pygame.display.update()
-	clock.tick(60)
+	def loop(self, clockTick):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit()
+
+			pygame.display.update()
+			self.clock.tick(clockTick)
+
+	def drawGrid(self):
+		pass
+
+game = Main(1000, 700, 20)
+game.initPygame('Pathfinder')
+game.loop(60)
