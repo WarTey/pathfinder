9d41b31ab7f179f785ee8599c2accdafb2be9ce3
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2022-07-03 21:39:01 -0400

    Display grid

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import NoReturn
 import pygame
 from sys import exit
 
@@ -10,22 +11,29 @@ class Main:
 	def initPygame(self, windowTitle):
 		pygame.init()
 		self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
+		self.screen.fill('White')
 		pygame.display.set_caption(windowTitle)
 		self.clock = pygame.time.Clock()
 
-	def loop(self, clockTick):
+	def drawGrid(self):
+		for x in range(self.screenWidth):
+			for y in range(self.screenHeight):
+				rect = pygame.Rect(x * self.blockSize, y * self.blockSize, self.blockSize, self.blockSize)
+				pygame.draw.rect(self.screen, 'Black', rect, 1)
+
+	def loop(self, clockTick = None):
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					exit()
 
+			self.drawGrid()
+
 			pygame.display.update()
-			self.clock.tick(clockTick)
+			if clockTick:
+				self.clock.tick(clockTick)
 
-	def drawGrid(self):
-		pass
-
-game = Main(1000, 700, 20)
+game = Main(1000, 700, 50)
 game.initPygame('Pathfinder')
-game.loop(60)
+game.loop()
